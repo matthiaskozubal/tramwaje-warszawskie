4bde6b220c56eae63a10bcd0b728e0804a2c24ba
matthiasphysicists <matthiasphysicists@gmail.com>
2016-04-17 12:21:05 +0200

    Learning code from master-branch
    First attempt to use this code, apikey stored in ignored file .env.

--- api.py
+++ api.py
@@ -1,31 +1,37 @@
-import urllib2
+
+import urllib.request
+from os.path import join, dirname
+import dotenv
 import json
 
+def get_credentials(var, filename='.env'):
+    path = join(dirname(__file__), filename)
+    return dotenv.get_variable(path, var) # returns properly only if line with APIKEY is not the last one, insert empty line after it
+
 def get_vehicles(data):
-    def get_vehicle(obj):
-        return dict([(x['key'], x['value']) for x in obj['values']])
-    return [get_vehicle(x) for x in data['result']]
+    def get_vehicle(item):
+        return dict( [(item_values['key'],item_values['value']) for item_values in item['values']] )
+    return [get_vehicle(item) for item in data['result']]
 
-def create_point(v_lat, v_long, v_number):
-    return {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [v_lat, v_long]
-        },
-        "properties": {
-          "marker-color": "#3ca0d3",
-          "marker-symbol": v_number
-        }
-      }
+def main():
+    response = urllib.request.urlopen('https://api.um.warszawa.pl/api/action/dbstore_get/?id=daeea0db-0f9a-498d-9c4f-210897daffd2&apikey=' + get_credentials('APIKEY')) # get page
+    html   = response.read().decode('utf-8') # decode page # http://stackoverflow.com/questions/23049767/parsing-http-response-in-python
+    data     = json.loads(html) # load string data to json # https://www.reddit.com/r/learnpython/comments/3nx9ch/json_load_vs_loads/
+    vehicles = get_vehicles(data) # take all vehicles, keys: tabor, linia, gps_dlug, ostatnia_aktualizacja, gps_szer
+    [print(vehicle['gps_dlug'],vehicle['gps_szer']) for vehicle in vehicles[:4]]
 
-response = urllib2.urlopen('https://api.um.warszawa.pl/api/action/dbstore_get/?id=daeea0db-0f9a-498d-9c4f-210897daffd2&apikey=f10fec28-bf17-4abd-94d7-7b42ac4ab33e')
-data = get_vehicles(json.load(response))
-geodata = [create_point(x['gps_dlug'], x['gps_szer'], x['linia']) for x in data]
-timedata = [x['ostatnia_aktualizacja'] for x in data]
+    with open('tramps_warsaw_json.txt', 'w') as output:
+        output.write(html)
 
-with open('tramwaje.json', 'w') as f:
-    f.write(json.dumps(geodata))
+if __name__ == '__main__':
+    main()
 
-print sorted(list(set(timedata)))
+
+# geodata = [create_point(x['gps_dlug'], x['gps_szer'], x['linia']) for x in data]
+# timedata = [x['ostatnia_aktualizacja'] for x in data]
+
+# with open('tramwaje.json', 'w') as f:
+#     f.write(json.dumps(geodata))
+
+# print sorted(list(set(timedata)))
 
